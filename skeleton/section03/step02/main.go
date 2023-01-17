77300fcbb5c35ddcdff133d487f398ff94bf3aef
TaichiOhmi <o6mi5.ta1@gmail.com>
2023-01-18 00:28:28 +0900

    sec3

--- skeleton/section03/step02/main.go
+++ skeleton/section03/step02/main.go
@@ -14,8 +14,12 @@ func main() {
 		all = append(all, n)
 	}
 
-	// 乱数の種をセットする
+	// fmt.Println(all)
+	// [2 3 4 5 6 7 8 9 10 11 12 13 14]
+
+	// 乱数の種を現在時刻で取得
 	t := time.Now().UnixNano()
+	// 乱数の種を設定
 	rand.Seed(t)
 
 	// 山札をシャッフルさせる
@@ -24,12 +28,14 @@ func main() {
 	})
 
 	// TODO: 山札の前方5枚を手札としcardsに入れる
-
+	cards := all[:5] // 0~4枚目までの5枚
 	// TODO: 6枚目以降を新しい山札とする
+	all = all[5:] // 5枚目以降
 
 	// 手札を表示させます
 	for i, n := range cards {
 		fmt.Printf("%d番目: ", i+1)
+		// この行以降で、n が、case の場合、
 		switch n {
 		case 11:
 			fmt.Println("J")
